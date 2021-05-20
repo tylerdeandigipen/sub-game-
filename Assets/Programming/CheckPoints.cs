78d9952fc6020c276cde446a413a55a1d382f9cc
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2021-05-20 13:07:38 -0700

    suffs

---
+++ Assets/Programming/CheckPoints.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoints : MonoBehaviour
+{
+    Vector3 spawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawn = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "checkpoint")
+        {
+            spawn = this.gameObject.transform.position;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            this.gameObject.transform.position = spawn;
+        }
+    }
+
+}
---
+++ Assets/Programming/FollowingCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class FollowingCamera : MonoBehaviour
+{
+    public GameObject Target;
+    public float smoothVal = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Target != null)
+        {
+            //Figure out where the target is
+            Vector3 newPos = Target.transform.position;
+            //maintain camera z level
+            newPos.z = transform.position.z;
+            //use linear interpolation to smoothly go to the target
+            transform.position = Vector3.Lerp(transform.position, newPos, smoothVal);
+        }
+    }
+}
