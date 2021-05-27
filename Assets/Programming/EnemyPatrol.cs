e07d59f2d440ebb66160e50f434ea7a24f5a104f
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2021-05-27 13:58:35 -0700

    Revert "Revert "Merge branch 'main' of https://github.com/tylerdeandigipen/sub-game- into main""
    This reverts commit b2e044244668be877a8a0ba21f7b187ea89c495f.

--- Assets/Programming/EnemyPatrol.cs
+++ Assets/Programming/EnemyPatrol.cs
@@ -35,7 +35,7 @@ public class EnemyPatrol : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Terrain")
+        if (collision.gameObject.tag == "Terrain")
         {
             print("Test");
             bIsGoingRight = !bIsGoingRight;
