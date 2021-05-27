07791b91c96cf57a3380e2941ad0547e5772ad96
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2021-05-27 13:43:29 -0700

    lava

--- Assets/Programming/CheckPoints.cs
+++ Assets/Programming/CheckPoints.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class CheckPoints : MonoBehaviour
 {
     Vector3 spawn;
+    Quaternion rotateion;
+    public GameObject lava;
+    LavaRise lavascript;
+    public bool check = true;
     // Start is called before the first frame update
     void Start()
     {
         spawn = this.gameObject.transform.position;
+        lavascript = lava.GetComponent<LavaRise>();
     }
 
     // Update is called once per frame
@@ -18,16 +23,19 @@ public class CheckPoints : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "checkpoint")
+        if (collision.gameObject.tag == "checkpoint" && check == true)
         {
             spawn = this.gameObject.transform.position;
+            rotateion = this.gameObject.transform.rotation;
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "enemy")
         {
+            this.gameObject.transform.rotation = rotateion;
             this.gameObject.transform.position = spawn;
+            lavascript.resetlava();
         }
     }
 
---
+++ Assets/Programming/LavaRise.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaRise : MonoBehaviour
+{
+    public GameObject relic;
+    public GameObject lava;
+    Vector3 lavapos;
+    bool rise = false;
+    public float speed;
+    public GameObject player;
+    CheckPoints checkies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lavapos = lava.gameObject.transform.position;
+        checkies = player.GetComponent<CheckPoints>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rise == true)
+        {
+            lava.gameObject.transform.Translate(0, Time.deltaTime * speed, 0);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        relic.SetActive(false);
+        checkies.check = false;
+        rise = true;
+    }
+    public void resetlava()
+    {
+        rise = false;
+        lava.gameObject.transform.position = lavapos;
+        relic.SetActive(true);
+    }
+
+}
